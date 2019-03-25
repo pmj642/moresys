c0067bb2de137f05a82cfd1b7619754557c1064d
Pranshu J <pmj642@gmail.com>
2019-03-25 18:45:28 +0530

    add view and template for movie_detail view

--- moresysapp/views.py
+++ moresysapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Movie
 
-# Create your views here.
+def movie_detail(request, movie_id):
+    from .recommender import getSimilar
+    similar_movies = getSimilar(movie_id)
+    return render(request, 'moresysapp/movie_detail.html', {'movie_id': movie_id, 'similar_movies': similar_movies})
