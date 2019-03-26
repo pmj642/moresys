55e3ea1090b550af0b68c2b09a567930bbd1b7f6
Pranshu J <pmj642@gmail.com>
2019-03-26 12:26:16 +0530

    add url and view for homepage

--- moresysapp/views.py
+++ moresysapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import Movie
+import json
+
+def home(request):
+    top_10_movies = Movie.objects.all().only("movie_id", "title", "poster_path", "runtime", "release_year").values()[:10]
+    return render(request, 'moresysapp/movie_list.html', {'movies': top_10_movies})
 
 def movie_detail(request, movie_id):
     from .recommender import getSimilar
